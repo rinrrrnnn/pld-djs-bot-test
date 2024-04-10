cba40de99d878532ef2ebaff7b1bd90093244667
擴音0 <139130906+rinrrrnnn@users.noreply.github.com>
2024-04-10 17:43:22 +0800

    update 4

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js'
-
+import vueInit from '@/core/vue'
 import dotenv from 'dotenv'
-
+vueInit()
 dotenv.config()
 // Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
 // It makes some properties non-nullable.
 client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`)
 });
 
 // Log in to Discord with your client's token
